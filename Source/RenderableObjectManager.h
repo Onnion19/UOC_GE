29e0fcac96fac696eb19e6a20e41c69ae832012b
Onnion19 <aleixrius19@gmail.com>
2024-10-25 22:41:31 +0200

    Implement Renderable manager

--- Source/RenderableObjectManager.h
+++ Source/RenderableObjectManager.h
@@ -3,19 +3,20 @@
 
 #include <vector>
 #include <d3d11.h>
+#include <memory>
 
 class CRenderableObject;
 
 class CRenderableObjectManager
 {
 protected:
-	std::vector<CRenderableObject *>				m_RenderableObjects;
+	std::vector<std::unique_ptr<CRenderableObject>> m_RenderableObjects;
 public:
 	CRenderableObjectManager();
 	virtual ~CRenderableObjectManager();
 	void Update(float ElapsedTime);
 	void Render(ID3D11DeviceContext *DeviceContext);
-	void AddRenderableObject(CRenderableObject *RenderableObject);
+	void AddRenderableObject(std::unique_ptr<CRenderableObject>&& RenderableObject);
 };
 
 #endif
