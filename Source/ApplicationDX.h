a73ca228843bebcbc31140f91981cce79f31af32
Onnion19 <aleixrius19@gmail.com>
2024-10-26 15:43:14 +0200

    Implement ApplicationDX

--- Source/ApplicationDX.h
+++ Source/ApplicationDX.h
@@ -2,22 +2,23 @@
 #define _APPLICATION_DX_H
 
 #include "defines.h"
+#include <memory>
 
 class CPlayer;
 
 class CApplicationDX
 {
 private:
-	CPlayer 				*m_Player;
-public:	
-	CApplicationDX();
+	std::unique_ptr<CPlayer> m_Player;
+public:
+	CApplicationDX() = default;
 	~CApplicationDX();
-	
+
 	LRESULT WINAPI MsgProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 	void Init(HWND hWnd, int Width, int Height);
 	void Update();
 	void Render();
-	CPlayer * GetPlayer() const {return m_Player;}
+	CPlayer* GetPlayer() const { return m_Player.get(); }
 };
 
 #endif
