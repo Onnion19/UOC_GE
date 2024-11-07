8636625a9d4a8316d7cd06d9dfebfaad4f8a1490
Onnion19 <aleixrius19@gmail.com>
2024-11-07 16:37:14 +0100

    Implement Mesh instance

--- Source/MeshInstance.cpp
+++ Source/MeshInstance.cpp
@@ -3,19 +3,21 @@
 #include "EffectManager.h"
 #include "UOCEngine.h"
 
-CMeshInstance::CMeshInstance(CFBXStaticMesh *StaticMesh)
-//TO DO : Llamar al constructor vacío de la clase base CRenderableObject
-//TO DO : Inicializar la variable miembro m_StaticMesh al valor de StaticMesh que recibimos por parámetro
+CMeshInstance::CMeshInstance(CFBXStaticMesh* StaticMesh)
+	: CRenderableObject()
+	, m_StaticMesh(StaticMesh)
+
 {
 }
 
-CMeshInstance::~CMeshInstance()
+void CMeshInstance::Render(ID3D11DeviceContext* DeviceContext)
 {
-}
+	auto effectManager = CUOCEngine::GetEngine()->GetEffectManager();
+	assert(effectManager);
 
-void CMeshInstance::Render(ID3D11DeviceContext *DeviceContext)
-{
-	//TO DO : Establecer la variable de matriz de mundo m_World que se encuentra en la variable estática m_ObjectConstantBufferParameters de la clase CEffectManager, debemos asignarle el valor que devuelve la matriz transpuesta utilizando la función de DirectX XMMatrixTranspose del GetTransform de la CRenderableObject de la que derivamos
-	//TO DO : Llamar al método SetObjectConstantBuffer del EffectManager que se encuentra en el motor
-	//TO DO : Llamar al método Render de la variable miembro m_StatiMesh
+	effectManager->m_ObjectConstantBufferParameters.m_World = DirectX::XMMatrixTranspose(GetTransform());
+	effectManager->SetObjectConstantBuffer(DeviceContext);
+
+	assert(m_StaticMesh);
+	m_StaticMesh->Render(DeviceContext);
 }
