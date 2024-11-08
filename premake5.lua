3a09591344bd44f607eef5cd60751531c40e6fa7
Onnion19 <aleixrius19@gmail.com>
2024-11-08 14:31:06 +0100

    Upadate premake script to include all libs

--- premake5.lua
+++ premake5.lua
@@ -1,25 +1,19 @@
 
 workspace "GameEngine"
     configurations { "Debug", "Release"}
-    platforms { "x64", "x86"}
-	defaultplatform ("x64")
+    platforms {"Win32", "x64"}
 	cdialect "C99"
     cppdialect "C++latest"
 	
 	startproject "Engine" 
 
-	defines{ "_XM_NO_INTRINSICS_"}
+	defines{ "_XM_NO_INTRINSICS_", "WIN32", "_WINDOWS"}
 
     filter "configurations:Debug"
         defines { "DEBUG" }
         symbols "On"
-
-    filter "configurations:Release"
-        defines { "NDEBUG" }
-        optimize "On"
-
-    filter { "platforms:x64" }
-        architecture "x86_64"
+		
+  
 		
 
     filter {}
@@ -35,8 +29,32 @@ workspace "GameEngine"
 		includedirs{"Source/include", "Source/src", "Source", "ThirdParty/expat", "ThirdParty/autodesk/include"}		
 		
 		files {"Source/**.h","Source/*.hpp", "Source/**.cpp", "ThirdParty/**.h", "ThirdParty/**.cpp"}
-		links {"libexpat64", "libfbxsdk" }
-		libdirs { "ThirdParty/expat", "ThirdParty/autodesk" }
+		links {
+		"d3d11",
+        "libfbxsdk-md",
+        "libxml2-md",
+        "zlib-md",
+        "winmm",
+        "libexpat64",
+        "D3DCompiler",
+        "dinput8",
+        "dxguid"}
+		libdirs { "ThirdParty/expat"}
+
+		
+		filter "configurations:Release"
+			defines { "NDEBUG" }
+			optimize "On"
+			libdirs { "ThirdParty/autodesk/release" }
+			
+		filter "configurations:Debug"
+			defines { "DEBUG" }
+			symbols "On"
+			libdirs { "ThirdParty/autodesk/debug" }
+			
+		filter { "platforms:x64" }
+			architecture "x86_64"
+			
 		
 		
 	
