84c1bbe457ec02e4e9858b61ab7ecf23a0160126
Onnion19 <aleixrius19@gmail.com>
2024-11-15 14:12:25 +0100

    Implement std::map for reuse vertices

--- Source/FBXStaticMesh.cpp
+++ Source/FBXStaticMesh.cpp
@@ -5,6 +5,7 @@
 #include "UOCEngine.h"
 #include "RenderManager.h"
 #include "TextureManager.h"
+#include <algorithm>
 
 CFBXStaticMesh::CFBXStaticMesh() : m_Effect(nullptr)
 {
@@ -49,89 +50,57 @@ bool CFBXStaticMesh::Generate(FbxNode* Node, FbxMesh* Mesh)
 	Mesh->GetMaterialIndices(&l_MaterialIndices);
 	std::vector<std::vector<UOC_POSITION_NORMAL_TEXTURE_VERTEX>> l_FullVertexs;
 	std::vector<std::vector<unsigned short>> l_Indices;
+
+	std::vector < std::unordered_map <int, unsigned short>> l_verticesMap;
 	for (int i = 0; i < m_Textures.size(); ++i)
 	{
 		l_FullVertexs.push_back(std::vector<UOC_POSITION_NORMAL_TEXTURE_VERTEX>());
 		l_Indices.push_back(std::vector<unsigned short>());
+		l_verticesMap.emplace_back(std::unordered_map <int, unsigned short>{});
 	}
 
 	for (int i = 0; i < Mesh->GetPolygonCount(); ++i)
 	{
 		int l_MaterialId = l_MaterialIndices->GetAt(i);
 		int l_PolygonVertexsCount = Mesh->GetPolygonSize(i);
-		int l_Index0 = Mesh->GetPolygonVertex(i, 0);
-		int l_Index1 = Mesh->GetPolygonVertex(i, 1);
-		int l_Index2 = Mesh->GetPolygonVertex(i, 2);
+		assert(l_PolygonVertexsCount == 3);
+		for (int vertice = 0; vertice < l_PolygonVertexsCount; vertice++)
+		{
+			int l_Index = Mesh->GetPolygonVertex(i, vertice);
 
-		FbxVector4 l_Vertex0 = l_Vertexs[l_Index0].mData;
-		FbxVector4 l_Vertex1 = l_Vertexs[l_Index1].mData;
-		FbxVector4 l_Vertex2 = l_Vertexs[l_Index2].mData;
+			auto vertice_iter = l_verticesMap[l_MaterialId].find(l_Index);
+			if (vertice_iter != l_verticesMap[l_MaterialId].end())
+			{
+				l_Indices[l_MaterialId].push_back(vertice_iter->second);
+				continue;
+			}
 
-		FbxVector4 l_Normal0 = l_Normals->GetAt(i * 3);
-		FbxVector4 l_Normal1 = l_Normals->GetAt(i * 3 + 1);
-		FbxVector4 l_Normal2 = l_Normals->GetAt(i * 3 + 2);
+			FbxVector4 l_Vertex = l_Vertexs[l_Index].mData;
+			FbxVector4 l_Normal = l_Normals->GetAt(i * 3 + vertice);
+			int l_TexCoordIndex = Mesh->GetTextureUVIndex(i, vertice, FbxLayerElement::eTextureDiffuse);
+			FbxVector2 l_TexCoord = l_TexCoords->GetAt(l_TexCoordIndex);			
+			UOC_POSITION_NORMAL_TEXTURE_VERTEX l_FullVertex(XMFLOAT3(-(float)l_Vertex.mData[0], (float)l_Vertex.mData[2], (float)l_Vertex.mData[1]), XMFLOAT3(-(float)l_Normal.mData[0], (float)l_Normal.mData[2], (float)l_Normal.mData[1]), XMFLOAT2((float)l_TexCoord.mData[0], 1.0f - (float)l_TexCoord.mData[1]));
+			l_FullVertexs[l_MaterialId].emplace_back(l_FullVertex);
+			l_verticesMap[l_MaterialId][l_Index] = static_cast<unsigned short>(l_FullVertexs[l_MaterialId].size()-1);
+			l_Indices[l_MaterialId].emplace_back((unsigned short)l_FullVertexs[l_MaterialId].size()-1);
 
-		int l_TexCoordIndex0 = Mesh->GetTextureUVIndex(i, 0, FbxLayerElement::eTextureDiffuse);
-		int l_TexCoordIndex1 = Mesh->GetTextureUVIndex(i, 1, FbxLayerElement::eTextureDiffuse);
-		int l_TexCoordIndex2 = Mesh->GetTextureUVIndex(i, 2, FbxLayerElement::eTextureDiffuse);
 
-		FbxVector2 l_TexCoord0 = l_TexCoords->GetAt(l_TexCoordIndex0);
-		FbxVector2 l_TexCoord1 = l_TexCoords->GetAt(l_TexCoordIndex1);
-		FbxVector2 l_TexCoord2 = l_TexCoords->GetAt(l_TexCoordIndex2);
+			if (l_FullVertex.Position.x < m_BBMin.x || i == 0)
+				m_BBMin.x = l_FullVertex.Position.x;
+			if (l_FullVertex.Position.y < m_BBMin.y || i == 0)
+				m_BBMin.y = l_FullVertex.Position.y;
+			if (l_FullVertex.Position.z < m_BBMin.z || i == 0)
+				m_BBMin.z = l_FullVertex.Position.z;
 
-		UOC_POSITION_NORMAL_TEXTURE_VERTEX l_FullVertex0(XMFLOAT3(-(float)l_Vertex0.mData[0], (float)l_Vertex0.mData[2], (float)l_Vertex0.mData[1]), XMFLOAT3(-(float)l_Normal0.mData[0], (float)l_Normal0.mData[2], (float)l_Normal0.mData[1]), XMFLOAT2((float)l_TexCoord0.mData[0], 1.0f - (float)l_TexCoord0.mData[1]));
-		UOC_POSITION_NORMAL_TEXTURE_VERTEX l_FullVertex1(XMFLOAT3(-(float)l_Vertex1.mData[0], (float)l_Vertex1.mData[2], (float)l_Vertex1.mData[1]), XMFLOAT3(-(float)l_Normal1.mData[0], (float)l_Normal1.mData[2], (float)l_Normal1.mData[1]), XMFLOAT2((float)l_TexCoord1.mData[0], 1.0f - (float)l_TexCoord1.mData[1]));
-		UOC_POSITION_NORMAL_TEXTURE_VERTEX l_FullVertex2(XMFLOAT3(-(float)l_Vertex2.mData[0], (float)l_Vertex2.mData[2], (float)l_Vertex2.mData[1]), XMFLOAT3(-(float)l_Normal2.mData[0], (float)l_Normal2.mData[2], (float)l_Normal2.mData[1]), XMFLOAT2((float)l_TexCoord2.mData[0], 1.0f - (float)l_TexCoord2.mData[1]));
-
-		l_FullVertexs[l_MaterialId].push_back(l_FullVertex0);
-		l_FullVertexs[l_MaterialId].push_back(l_FullVertex1);
-		l_FullVertexs[l_MaterialId].push_back(l_FullVertex2);
-		l_Indices[l_MaterialId].push_back((unsigned short)l_Indices[l_MaterialId].size());
-		l_Indices[l_MaterialId].push_back((unsigned short)l_Indices[l_MaterialId].size());
-		l_Indices[l_MaterialId].push_back((unsigned short)l_Indices[l_MaterialId].size());
-
-		if (l_FullVertex0.Position.x < m_BBMin.x || i == 0)
-			m_BBMin.x = l_FullVertex0.Position.x;
-		if (l_FullVertex0.Position.y < m_BBMin.y || i == 0)
-			m_BBMin.y = l_FullVertex0.Position.y;
-		if (l_FullVertex0.Position.z < m_BBMin.z || i == 0)
-			m_BBMin.z = l_FullVertex0.Position.z;
-
-		if (l_FullVertex1.Position.x < m_BBMin.x)
-			m_BBMin.x = l_FullVertex1.Position.x;
-		if (l_FullVertex1.Position.y < m_BBMin.y)
-			m_BBMin.y = l_FullVertex1.Position.y;
-		if (l_FullVertex1.Position.z < m_BBMin.z)
-			m_BBMin.z = l_FullVertex1.Position.z;
-
-		if (l_FullVertex2.Position.x < m_BBMin.x)
-			m_BBMin.x = l_FullVertex2.Position.x;
-		if (l_FullVertex2.Position.y < m_BBMin.y)
-			m_BBMin.y = l_FullVertex2.Position.y;
-		if (l_FullVertex2.Position.z < m_BBMin.z)
-			m_BBMin.z = l_FullVertex2.Position.z;
-
-		if (l_FullVertex0.Position.x > m_BBMax.x || i == 0)
-			m_BBMax.x = l_FullVertex0.Position.x;
-		if (l_FullVertex0.Position.y > m_BBMax.y || i == 0)
-			m_BBMax.y = l_FullVertex0.Position.y;
-		if (l_FullVertex0.Position.z > m_BBMax.z || i == 0)
-			m_BBMax.z = l_FullVertex0.Position.z;
-
-		if (l_FullVertex1.Position.x > m_BBMax.x)
-			m_BBMax.x = l_FullVertex1.Position.x;
-		if (l_FullVertex1.Position.y > m_BBMax.y)
-			m_BBMax.y = l_FullVertex1.Position.y;
-		if (l_FullVertex1.Position.z > m_BBMax.z)
-			m_BBMax.z = l_FullVertex1.Position.z;
-
-		if (l_FullVertex2.Position.x > m_BBMax.x)
-			m_BBMax.x = l_FullVertex2.Position.x;
-		if (l_FullVertex2.Position.y > m_BBMax.y)
-			m_BBMax.y = l_FullVertex2.Position.y;
-		if (l_FullVertex2.Position.z > m_BBMax.z)
-			m_BBMax.z = l_FullVertex2.Position.z;
+			if (l_FullVertex.Position.x > m_BBMax.x || i == 0)
+				m_BBMax.x = l_FullVertex.Position.x;
+			if (l_FullVertex.Position.y > m_BBMax.y || i == 0)
+				m_BBMax.y = l_FullVertex.Position.y;
+			if (l_FullVertex.Position.z > m_BBMax.z || i == 0)
+				m_BBMax.z = l_FullVertex.Position.z;
+		}
 	}
+
 	m_Position = XMFLOAT3((m_BBMin.x + m_BBMax.x) / 2.0f, (m_BBMin.y + m_BBMax.y) / 2.0f, (m_BBMin.z + m_BBMax.z) / 2.0f);
 	for (size_t i = 0; i < m_Textures.size(); ++i)
 	{
